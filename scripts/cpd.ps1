444010507c0c2a879376317c256809e5599effbb
SirMathhman <sirmathhman@gmail.com>
2026-02-12 14:06:18 -0500

    Add script to scan for duplicate string subsets in C files

--- scripts/cpd.ps1
+++ scripts/cpd.ps1
@@ -1,1 +1,2 @@
-pmd cpd src --language cpp --minimum-tokens 35 --ignore-literals --ignore-identifiers
+pmd cpd src --language cpp --minimum-tokens 35 --ignore-literals --ignore-identifiers;
+./scripts/scan-duplicate-string-subsets.ps1 -Paths @("./src/*.c", "./src/*.h", "./tests/*.c") -MinimumLength 21;
