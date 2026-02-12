5d02eddeb55fdf7024ecd2195bd92788b38e9d8c
SirMathhman <sirmathhman@gmail.com>
2026-02-12 14:06:49 -0500

    Update precommit script to include duplicate string subset scan for C files

--- scripts/cpd.ps1
+++ scripts/cpd.ps1
@@ -1,2 +1,1 @@
 pmd cpd src --language cpp --minimum-tokens 35 --ignore-literals --ignore-identifiers;
-./scripts/scan-duplicate-string-subsets.ps1 -Paths @("./src/*.c", "./src/*.h", "./tests/*.c") -MinimumLength 21;
--- scripts/precommit.ps1
+++ scripts/precommit.ps1
@@ -1,2 +1,3 @@
 ./scripts/test.ps1;
 ./scripts/cpd.ps1;
+./scripts/scan-duplicate-string-subsets.ps1 -Paths @("./src/*.c") -MinimumLength 20;
