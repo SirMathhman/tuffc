24a83e49e5343f2e1bdf56b3d9e81547f357e28f
SirMathhman <sirmathhman@gmail.com>
2026-02-12 21:41:07 -0500

    Reorder test execution and error handling in test script

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,2 +1,5 @@
 clang ./src/main.c ./src/common.c ./tests/test.c -Werror -o ./dist/test.exe;
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
 ./dist/test.exe;
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
