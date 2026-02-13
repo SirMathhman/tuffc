97556bad7c28bc803f03afcac4bec929ec284f73
SirMathhman <sirmathhman@gmail.com>
2026-02-12 23:31:41 -0500

    Improve precommit script to handle exit codes consistently for all commands

--- scripts/precommit.ps1
+++ scripts/precommit.ps1
@@ -1,8 +1,19 @@
 cargo test
-if (LastExitCode -ne 0) {
-    exit 1
+if ($LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
 }
 
 pmd cpd src --language rust --minimum-tokens 35
+if ($LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
+}
+
 cargo clippy
+if ($LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
+}
+
 cargo fmt
+if ($LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
+}
