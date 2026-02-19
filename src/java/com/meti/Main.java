9dfde8fdbeffa42b3eec9f6726e1fad8e7feb9b4
SirMathhman <sirmathhman@gmail.com>
2026-02-19 09:32:47 -0500

    Extern let

--- src/java/com/meti/Main.java
+++ src/java/com/meti/Main.java
@@ -5,9 +5,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Main {
+	private static final Map<List<String>, List<String>> imports = new HashMap<List<String>, List<String>>();
+
 	public static void main(String[] args) {
 		try {
 			final var input = Files.readString(createPath("java"));
@@ -38,12 +46,42 @@ public class Main {
 			segments.add(buffer.toString());
 		}
 
-		return segments
-				.stream()
-				.filter(slice -> !slice.startsWith("package "))
-				.map(String::strip)
-				.map(slice -> slice + System.lineSeparator())
-				.collect(Collectors.joining());
+		final var joinedRoot = segments.stream().map(Main::compileRootSegment).flatMap(Optional::stream).toList();
+
+		final var newImports = imports.entrySet().stream().map(entry -> {
+			final var key = entry.getKey();
+			final var values = entry.getValue();
+			return "extern let { " + String.join(", ", values) + " } = " + String.join("::", key) + ";";
+		}).toList();
+
+		final var outputSegments = new ArrayList<String>(newImports);
+		outputSegments.addAll(joinedRoot);
+		return outputSegments.stream().map(slice -> slice + System.lineSeparator()).collect(Collectors.joining());
+	}
+
+	private static Optional<String> compileRootSegment(String input) {
+		final var stripped = input.strip();
+		if (stripped.isEmpty() || stripped.startsWith("package ")) {
+			return Optional.empty();
+		}
+
+		if (stripped.startsWith("import ") && stripped.endsWith(";")) {
+			final var i = stripped.lastIndexOf(".");
+			if (i >= 0) {
+				final var substring = stripped.substring("import ".length(), i);
+				final var leaf = stripped.substring(i + 1, stripped.length() - 1);
+				final var split = substring.split(Pattern.quote("."));
+				final var namespace = Arrays.asList(split);
+				if (!imports.containsKey(namespace)) {
+					imports.put(namespace, new ArrayList<String>());
+				}
+
+				imports.get(namespace).add(leaf);
+				return Optional.empty();
+			}
+		}
+
+		return Optional.of(stripped + System.lineSeparator());
 	}
 
 	private static Path createPath(String extension) {
