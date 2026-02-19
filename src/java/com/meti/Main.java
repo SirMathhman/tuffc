ed4acb012ecb84998b93540bd71e9db4978c1a40
SirMathhman <sirmathhman@gmail.com>
2026-02-19 10:02:35 -0500

    feat: implement Rule interface and add StripRule and InfixRule classes for enhanced lexing capabilities

--- src/java/com/meti/Main.java
+++ src/java/com/meti/Main.java
@@ -14,6 +14,11 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Main {
+	private interface Rule {
+
+		Optional<MapNode> lex(String value);
+	}
+
 	private static final class MapNode {
 		private final Map<String, String> strings = new HashMap<String, String>();
 
@@ -32,17 +37,37 @@ public class Main {
 		}
 	}
 
-	private record StringRule(String key) {
-		private Optional<MapNode> lex(String value) {
+	private record StringRule(String key) implements Rule {
+		@Override
+		public Optional<MapNode> lex(String value) {
 			return Optional.of(new MapNode().withString(this.key(), value));
 		}
 	}
 
-	private record SuffixRule(StringRule childRule, String suffix) {
-		private Optional<MapNode> lex(String slice) {
+	private record SuffixRule(StringRule childRule, String suffix) implements Rule {
+		@Override
+		public Optional<MapNode> lex(String slice) {
 			if (!slice.endsWith(this.suffix())) {return Optional.empty();}
 			final var body = slice.substring(0, slice.length() - this.suffix().length());
 			return this.childRule().lex(body);
+		}
+	}
+
+	private record StripRule(StringRule childRule) implements Rule {
+		@Override
+		public Optional<MapNode> lex(String substring) {
+			return this.childRule().lex(substring.strip());
+		}
+	}
+
+	private record InfixRule(Rule leftRule, String infix, Rule rightRule) implements Rule {
+		@Override
+		public Optional<MapNode> lex(String input) {
+			final var i1 = input.indexOf(this.infix());
+			if (i1 < 0) {return Optional.empty();}
+			final var substring = input.substring(0, i1);
+			final var afterName = input.substring(i1 + this.infix().length());
+			return this.leftRule().lex(substring).flatMap(inner -> this.rightRule().lex(afterName).map(inner::merge));
 		}
 	}
 
@@ -126,17 +151,18 @@ public class Main {
 	}
 
 	private static Optional<String> compileClass(String stripped) {
-		final var i = stripped.indexOf("class ");
-		if (i < 0) {return Optional.empty();}
-		final var afterKeyword = stripped.substring(i + "class ".length()).strip();
-		final var i1 = afterKeyword.indexOf("{");
-		if (i1 < 0) {return Optional.empty();}
-		final var name = afterKeyword.substring(0, i1).strip();
-		final var afterName = afterKeyword.substring(i1 + 1);
-		final var name1 = new MapNode().withString("name", name);
-		return new SuffixRule(new StringRule("body"), "}").lex(afterName).flatMap(node -> {
-			return generateObject(name1.merge(node));
-		});
+		return getString(stripped).flatMap(Main::generateObject);
+	}
+
+	private static Optional<MapNode> getString(String stripped) {
+		return getRule().lex(stripped);
+	}
+
+	private static InfixRule getRule() {
+		final var modifiers = new StringRule("modifiers");
+		final var name = new StripRule(new StringRule("name"));
+		final var body = new StringRule("body");
+		return new InfixRule(modifiers, "class ", new InfixRule(name, "{", new SuffixRule(body, "}")));
 	}
 
 	private static Optional<String> generateObject(MapNode mapNode) {
