1f57262fbafe8d9e0e30a13f28ff082679889baa
SirMathhman <sirmathhman@gmail.com>
2026-02-19 09:14:19 -0500

    Cleanup

--- src/main/c/panic.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-// Tuff C panic entry points. Implemented in panic.c.
-
-#include <stdint.h>
-
-int64_t panic(int64_t msg);
-int64_t panic_with_code(int64_t code, int64_t msg, int64_t reason, int64_t fix);
--- src/main/c/string-builder.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-// Tuff C StringBuilder operations. Implemented in string-builder.c.
-
-#include <stdint.h>
-
-int64_t sb_new(void);
-int64_t sb_append(int64_t sb_val, int64_t s);
-int64_t sb_append_char(int64_t sb_val, int64_t code);
-int64_t sb_build(int64_t sb_val);
