6b0699cdb47ddf2dfb717cdd3ac972b8fa410b31
SirMathhman <sirmathhman@gmail.com>
2026-03-20 15:11:02 -0400

    Nuke

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-console.log("Hello, Tuff!");
-
-// TODO: implement interpretation logic
-export function interpretTuff(_source: string): number {
-  if (_source === "") return 0;
-  if (_source === "100") return 100;
-  if (_source === "100U8") return 100;
-  return 0;
-}
